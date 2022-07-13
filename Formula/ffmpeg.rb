357d1d3dca83eaace55a8e17e68b1f5639541f80
eko5624 <562433403@qq.com>
2022-07-13 17:50:04 +0800

    Update ffmpeg.rb

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -46,6 +46,7 @@ class Ffmpeg < Formula
   depends_on "frei0r"
   depends_on "lame"
   depends_on "libass"
+  depends_on "libjxl"
   depends_on "libvorbis"
   depends_on "libvpx"
   depends_on "opus"
