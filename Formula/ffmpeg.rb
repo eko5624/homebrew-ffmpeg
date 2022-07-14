cba7b981cb2951705b48b19d116feb58f182383f
eko5624 <562433403@qq.com>
2022-07-14 14:20:36 +0800

    Update ffmpeg.rb

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -46,7 +46,7 @@ class Ffmpeg < Formula
   depends_on "frei0r"
   depends_on "lame"
   depends_on "libass"
-  depends_on "eko5624/ffmpeg/jpeg-xl"
+  depends_on "eko5624/ffmpeg/jxl"
   depends_on "libvorbis"
   depends_on "libvpx"
   depends_on "opus"
