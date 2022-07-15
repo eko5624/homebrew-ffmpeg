7e08e90f3dcd149809de74be9a274b4985471e17
eko5624 <562433403@qq.com>
2022-07-15 13:13:04 +0800

    Update jxl.rb

--- Formula/jxl.rb
+++ Formula/jxl.rb
@@ -1,19 +1,10 @@
-class Jxl < Formula
+class JpegXl < Formula
   desc "New file format for still image compression"
   homepage "https://jpeg.org/jpegxl/index.html"
   url "https://github.com/libjxl/libjxl/archive/v0.6.1.tar.gz"
   sha256 "ccbd5a729d730152303be399f033b905e608309d5802d77a61a95faa092592c5"
   license "BSD-3-Clause"
   head "https://github.com/libjxl/libjxl.git", branch: "main"
-
-  bottle do
-    sha256 cellar: :any,                 arm64_monterey: "299002d5c10f0009bcdf7b2f5c4d544e4b84fc36dd109a0b3dd5eee780dbb5f7"
-    sha256 cellar: :any,                 arm64_big_sur:  "ad8d741fe5ff44ed35f407ff6f692763c937d74e51156563fb3e8245ac534898"
-    sha256 cellar: :any,                 monterey:       "a311fe41c7dc098c82269b42fd819ef20ce7248f9df444ce4890576005124204"
-    sha256 cellar: :any,                 big_sur:        "77882f055f5509900e8f913352386201a3cb57cbae04f7de254416416d9638d7"
-    sha256 cellar: :any,                 catalina:       "ef47fc0d2de7c6623c1504d6df8b9b041763bec532c4f907793f4ffedc6f3050"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "011b3fd5a6bfdd494edaddf64cb4432cadf6ad71adf24201a9555848eb2717c4"
-  end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
@@ -39,12 +30,12 @@ class Jxl < Formula
   # https://github.com/libjxl/libjxl/tree/v#{version}/third_party
   resource "highway" do
     url "https://github.com/google/highway.git",
-        revision: "e2397743fe092df68b760d358253773699a16c93"
+        revision: "9c775721c1531fd1cf11cf52bbfde4d076691628"
   end
 
-  resource "lodepng" do
-    url "https://github.com/lvandeve/lodepng.git",
-        revision: "48e5364ef48ec2408f44c727657ac1b6703185f8"
+  resource "libpng" do
+    url "https://github.com/glennrp/libpng.git",
+        revision: "a40189cf881e9f0db80511c382292a5604c3c3d1"
   end
 
   resource "sjpeg" do
@@ -58,7 +49,13 @@ class Jxl < Formula
   end
 
   def install
-    resources.each { |r| r.stage buildpath/"third_party"/r.name }
+    opts  = Hardware::CPU.arm? ? "-mcpu=native " : "-march=native -mtune=native "
+    opts += "-Ofast "
+    ENV.append "CFLAGS",      opts
+    ENV.append "CPPFLAGS",    opts
+    ENV.append "CXXFLAGS",    opts
+
+    resources.each { |r| r.stage buildpath/"third_party"/r.download_name }
     mkdir "build" do
       # disable manpages due to problems with asciidoc 10
       system "cmake", "..", "-DBUILD_TESTING=OFF",
